cc8a119c87b3867592db11c400633f2c1a36be09
hi991016 <miureus@gmail.com>
2024-11-01 21:28:45 +0700

    update code

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,37 +2,40 @@
 
 // ===== init =====
 const init = () => {
-    // # app-height
-    appHeight();
-    // # lenis
-    initLenis();
-    // # firstview
-    const swiperFv = new Swiper("[data-fv-swiper]", {
-        effect: "fade",
-        speed: 2000,
-        autoplay: {
-            delay: 1500,
-        },
-    });
-}
+  // # app-height
+  appHeight();
+  // # lenis
+  initLenis();
+  // # firstview
+  const swiperFv = new Swiper("[data-fv-swiper]", {
+    effect: "fade",
+    speed: 2000,
+    autoplay: {
+      delay: 1500,
+    },
+  });
+};
 
 const appHeight = () => {
-    const doc = document.documentElement;
-    doc.style.setProperty(
-        "--app-height",
-        `${document.documentElement.clientHeight}px`
-    );
+  const doc = document.documentElement;
+  doc.style.setProperty(
+    "--app-height",
+    `${document.documentElement.clientHeight}px`
+  );
 };
 window.addEventListener("resize", appHeight);
 
 const initLenis = () => {
-    const lenis = new Lenis({});
-    lenis.on('scroll', (e) => { });
-    function raf(time) {
-        lenis.raf(time);
-        requestAnimationFrame(raf);
-    }
+  const lenis = new Lenis({
+    lerp: 0.05,
+    smoothWheel: true,
+  });
+  lenis.on("scroll", (e) => {});
+  function raf(time) {
+    lenis.raf(time);
     requestAnimationFrame(raf);
-}
+  }
+  requestAnimationFrame(raf);
+};
 
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
